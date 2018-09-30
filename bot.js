0887159df286e2983814f8399820e45ec83f55bc
Ronsosner <43617643+Ronsosner@users.noreply.github.com>
2018-09-30 11:22:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,6 @@ bot.on("message", function(message) {
             message.channel.sendEmbed(embed);
             break;
         case "noticeme":
-            message.delete;
             message.channel.sendMessage(message.author.toString() + "Staff");
             break;
         case "removerole":
